7cf06df0ba33f84af1053c556698c980ce2996b6
David Garber <davidgarber@gmail.com>
2015-05-09 20:01:48 -0700

    returns 'ping' if the number is divisible by 3

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -4,7 +4,11 @@ class Fixnum
     new_array = []
     self.times() do |time|
       var = time.+(1)
-      new_array.push(var)
+      if var.%(3) == 0
+        new_array.push('ping')
+      elsif
+        new_array.push(var)
+      end
     end
     new_array
   end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,9 @@ describe("ping_pong") do
   it("creates an array of [1,2] when 2 is entered") do
     expect((2).ping_pong).to(eq([1,2]))
   end
+
+
+  it("returns 'ping' if the number is divisible by 3") do
+    expect((3).ping_pong).to(eq([1, 2, 'ping']))
+  end
 end
